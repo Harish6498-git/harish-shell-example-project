bf7c8432598087ad130c0229b3ac37f08af6c638
harish <harish@example.com>
2025-02-24 01:23:44 +0530

    save changes

--- example2.sh
+++ example2.sh
@@ -6,10 +6,16 @@
 
 # Multiplication Function
 
+<<<<<<< HEAD
 
 # This is test function
 
 # test function
+=======
+# Division Function
+
+>>>>>>> 7539c32 (division function)
 x=a+b
 y=a-b
 z=a*b
+w=a%b
